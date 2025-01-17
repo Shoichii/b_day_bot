9f4d6f580d6f5010377bbfc2d833013326d25066
helldiver <Shoichii>
2025-01-17 09:17:07 +0300

    поправка

--- pic_api.py
+++ pic_api.py
@@ -5,7 +5,8 @@ import env
 def random_image():
     '''Получение рандомной картинки по API'''
     url = (f"https://api.unsplash.com/photos/random?client_id={env.CLIENT_ID_UNSPLASH_SERVICE}"
-           "id&collections=9155171")
+           "&collections=9155171")
+    print(url)
     headers = {'Authorization': 'key'}
     response = requests.request("GET", url, headers=headers)
     return response.json()['urls']['regular']
